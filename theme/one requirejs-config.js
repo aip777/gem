f0604026175824df227552b62f44961c5d75abad
aip777 <ariful.islamk777@gmail.com>
2020-01-29 14:45:01 +0600

    Header Change

--- theme/one requirejs-config.js
+++ theme/one requirejs-config.js
@@ -2,13 +2,9 @@ var config = {
 map: {
     '*': {
 
+        modernizer        : 'Magento_Theme/js/modernizr.js'
+        bootstrapcdn: "https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
 
-
-
-        modernizer        : 'Magento_Theme/js/modernizr.js'}
-
-
-
-
+}
     }
 };
