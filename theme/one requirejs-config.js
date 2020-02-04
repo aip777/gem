6a3f017350fd4f30e312a5927d85c8736f35ba76
aip777 <ariful.islamk777@gmail.com>
2020-02-04 17:45:42 +0600

    bootstrap

--- theme/one requirejs-config.js
+++ theme/one requirejs-config.js
@@ -3,7 +3,7 @@ map: {
     '*': {
 
         modernizer        : 'Magento_Theme/js/modernizr.js'
-        bootstrapcdn: "https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+//        bootstrapcdn: "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"
 
 }
     }
